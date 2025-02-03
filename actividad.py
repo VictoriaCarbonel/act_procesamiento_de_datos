3326f8a4d0410cf547c974d3122df1380dd96225
Victoria <victoriamcarbonel@gmail.com>
2025-02-03 11:49:56 -0300

    prueba

--- actividad.py
+++ actividad.py
@@ -1,1 +1,11 @@
 
+#Parte 1
+
+"""Deben generar una matriz (de 3 filas x 4 columnas) denominada empleado_01
+conteniendo los siguientes datos (todos de tipo entero)
+Importante: Implementar la matriz como lista de filas"""
+
+import numpy as np
+
+empleado_01 = np.array([[20222333,45,2,20000],[33456234,40,0,25000],[45432345,41,1,10000]])
+
